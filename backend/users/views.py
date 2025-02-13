6bb1152fbad4eb802377d8ab41e910f79cda20e9
DaarcyDev <liam_alvarez@hotmail.com>
2025-02-12 23:57:57 -0600

    Implement API service and update views for test message retrieval (Connection between Django and Angular)

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.http import HttpResponse
+
 # Create your views here.
 
 @api_view(['GET'])
 def test_api(request):
-	return Response({"message": "Hello, world!"})
+	return Response({"message": "Hello, world! from Django test"})
